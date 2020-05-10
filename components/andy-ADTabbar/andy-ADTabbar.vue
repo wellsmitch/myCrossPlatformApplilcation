0c3b821c1975b93002cf1cef396dfb3badac536b
songyubin <1970083855@qq.com>
2020-05-10 19:02:49 +0800

    第一次提交

---
+++ components/andy-ADTabbar/andy-ADTabbar.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="tabbar" :style="{backgroundColor: backgroundColor, height: setTabHeight + 'upx'}">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	import Vue from 'vue'
+	
+	export default {
+		name: 'adTabbar',
+		components: {},
+		props: {
+			// 背景色
+			backgroundColor: {
+				type: String,
+				default: "#FFFFFF"
+			},
+			// tabbar高度
+			tabHeight: {
+				type: [String, Number],
+				default: 130
+			}
+		},
+		computed: {
+			// 设置Tabbar高度
+			setTabHeight() {
+				let info = 0;
+			// #ifdef APP-PLUS
+			info = plus.navigator.getSafeAreaInsets().bottom;
+			// #endif
+				
+				let tabh = parseFloat(this.tabHeight) + info / 2;
+				// Vue.prototype.TabBarHeight = tabh;
+				// console.log('Vue.prototype.TabBarHeight: ' + Vue.prototype.TabBarHeight)
+				return tabh;
+			},
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+	.tabbar {
+		/*foot*/
+		position: fixed;
+		width: 100%;
+		min-height: 100upx;
+		bottom: 0;
+		z-index: 1024;
+		display: flex;
+		
+		/*cu-bar*/
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		
+		/*tabbar*/
+		padding: 0;
+		/* height: calc(130upx + env(safe-area-inset-bottom) / 2); */
+		padding-bottom: calc(env(safe-area-inset-bottom) / 2);
+		
+		/*shadow*/
+		box-shadow: 0 -1upx 6upx rgba(0, 0, 0, 0.1);
+		
+		color: #666666;
+		
+		/*other*/
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="baseView" id="baseView">
+		<page1 :style="{'display':curPage == 'page1' ?'block':'none'}"></page1>
+		<page2 :style="{'display':curPage == 'page2' ?'block':'none'}"></page2>
+		<page3 :style="{'display':curPage == 'page3' ?'block':'none'}"></page3>
+		<page4 :style="{'display':curPage == 'page4' ?'block':'none'}"></page4>
+		<adTabbar :tabHeight='tabHeight'>
+			<adTabbarItem text="首页" dataCur="page1" class="maxWidth" @click="navClick" :textColor="curPage=='page1'? '#DF421D':'#9B9B9B'" :icon="'../../static/andy-ADTabbar/home' + [curPage=='page1'?'-hover':''] + '.png'"></adTabbarItem>
+			<adTabbarItem text="订单" dataCur="page2" class="maxWidth" @click="navClick" :textColor="curPage=='page2'? '#DF421D':'#9B9B9B'" :icon="'../../static/andy-ADTabbar/order' + [curPage=='page2'?'-hover':''] + '.png'"></adTabbarItem>
+			<!--自定义中间View，可以注释掉就是正常4个Tab菜单-->
+			<view class="middleItem" @click="middleClick">
+				<view class="buttonView">+</view>
+				<text>自定义</text>
+			</view>
+			<adTabbarItem text="图表" dataCur="page3" class="maxWidth" @click="navClick" :textColor="curPage=='page3'? '#DF421D':'#9B9B9B'" :icon="'../../static/andy-ADTabbar/chart' + [curPage=='page3'?'-hover':''] + '.png'"></adTabbarItem>
+			<adTabbarItem text="我的" dataCur="page4" class="maxWidth" @click="navClick" :textColor="curPage=='page4'? '#DF421D':'#9B9B9B'" :icon="'../../static/andy-ADTabbar/my' + [curPage=='page4'?'-hover':''] + '.png'"></adTabbarItem>
+		</adTabbar>
+	</view>
+</template>
+
+<script>
+	import AppConfig from '@/common/appConfig.js';
+	import adTabbar from '@/components/andy-ADTabbar/andy-ADTabbar.vue';
+	import adTabbarItem from '@/components/andy-ADTabbar/andy-ADTabbarItem.vue';
+	
+	export default {
+		components: {
+		    adTabbar,
+			adTabbarItem
+		},
+		data() {
+			return {
+				curPage: 'page1',
+				tabHeight: AppConfig.TabbarHeight,
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			// 导航栏切换
+			navClick: function(e) {
+				this.curPage = e.currentTarget.dataset.cur
+			},
+			// 中间按钮点击
+			middleClick: function(e) {
+				uni.navigateTo({
+				    url: 'page5',
+				    animationType: 'slide-in-bottom',
+				    animationDuration: 200
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+	.middleItem {
+		min-width: calc(100vw / 5);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		font-size: 22upx;
+	}
+	.maxWidth {
+		width: 100%;
+	}
+	.buttonView {
+		width: 100upx;
+		height: 100upx;
+		border-radius: 50upx;
+		margin-top: -60upx;
+		background-color: #349E40;
+		margin-bottom: 10upx;
+		
+		color: #FFFFFF;
+		line-height: 100upx;
+		text-align: center;
+		font-size: 50upx;
+	}
+</style>
